df24205b6fcebc3b882b250cea5a9e83f1b66f1b
shaneypricken <162596806+shaneypricken@users.noreply.github.com>
2024-09-10 09:11:29 -0400

    Add files via upload

---
+++ shane.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int a;
+float b;
+char c;
+bool d;
+string e;
+int main() {
+    a = 37;
+    b = 43.86;
+    c = 'd';
+    d = false;
+    e = "Chrizz";
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+}
